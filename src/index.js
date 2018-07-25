bacb1b8185fe2d11e423b81c3f3b0fb01bdda5f0
Diana Voyanets <diana.voyanets@gmail.com>
2018-07-25 12:12:40 +0300

    Implemented closestCityToLocation with unit tests

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ City.prototype.getCode = function () {
     var splited = this.name.split(',');
     
     if (splited < 1) {
-        throw new Error('Сity name is not contain the city code');
+        throw new Error('Сity name is not contain the code');
     }
 
     var code = splited[1].trim(); 
@@ -36,11 +36,41 @@ Map.prototype.getEasternmostCity = function () {
 }
 
 Map.prototype.getSouthernmostCity = function () {
-    
+
 }
 
 Map.prototype.getWesternmostCity = function () {
 
+}
+
+function distanceBetweenGeocodes(lat1, lon1, lat2, lon2) {
+
+    function toRadians(value) { 
+        return value * Math.PI / 180; 
+    }
+    
+    var rad1 = toRadians(lat1) 
+        rad2 = toRadians(lat2), 
+        sigma = toRadians(lon2 - lon1), 
+        R = 6371e3;
+
+    var distance = Math.acos(Math.sin(rad1) * Math.sin(rad2) + Math.cos(rad1) * Math.cos(rad2) * Math.cos(sigma)) * R;
+
+    return distance;
+}
+
+Map.prototype.closestCityToLocation = function (longitude, latitude) {
+
+    var citiesDistances = this.cities
+        .map((city) => {
+            return {
+                city: city, 
+                distance: distanceBetweenGeocodes(longitude, latitude, city.latitude, city.longitude)
+            }
+        });
+
+    var citiesDistances = citiesDistances.reduce((prev, curr) => prev.distance < curr.distance ? prev : curr);
+    return citiesDistances.city;
 }
 
 Map.prototype.getCityCodes = function () {
--- tests.js
+++ tests.js
@@ -17,4 +17,19 @@ describe('Map', function() {
 
         assert.deepEqual(cityCodes, 'TN,NY,GA,CO,WA,CA,TN');
     });
+
+    it('closestCityToLocation', function() {
+        const map = new Map([
+            new City("Nashville, TN", 36.17, -86.78),
+            new City("New York, NY", 40.71, -74.00),
+            new City("Atlanta, GA", 33.75, -84.39),
+            new City("Denver, CO", 39.74, -104.98),
+            new City("Seattle, WA", 47.61, -122.33),
+            new City("Los Angeles, CA", 34.05, -118.24),
+            new City("Memphis, TN", 35.15, -90.05)
+        ]);
+
+        const city = map.closestCityToLocation(34.03, -110);
+        assert.deepEqual(city.name, 'Los Angeles, CA');
+    });
 });
